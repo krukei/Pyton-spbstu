b1d97d375ea7cd100bc299c8df4809b7bb49fe57
krukei <lizakruk2006@gmail.com>
2026-02-16 01:40:23 +0300

    task_2.py

--- Лабораторная работа 2/task_2.py
+++ Лабораторная работа 2/task_2.py
@@ -48,10 +48,6 @@ class Library:
                 return index
         raise ValueError("Книги с запрашиваемым id не существует")
 
-
-
-        return self.book.id
-
 if __name__ == '__main__':
     empty_library = Library()  # инициализируем пустую библиотеку
     print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
